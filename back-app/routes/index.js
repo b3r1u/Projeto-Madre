5abf1ca0e1f7edebbd7599bfff5a982e7fd20dae
Rafa Cavalcanti <149629955+RafaCavalcant1@users.noreply.github.com>
2024-06-09 19:32:46 +0000

    requisições HTTP de users

--- back-app/routes/index.js
+++ back-app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'estou no back' });
+  res.send({ title: 'estou no back' });
 });
 
 module.exports = router;
--- back-app/routes/users.js
+++ back-app/routes/users.js
@@ -1,9 +1,114 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const db = new sqlite3.Database('./database/database.db')
+
+db.run(`CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+   username TEXT, 
+   email TEXT UNIQUE, 
+   cpf TEXT UNIQUE,
+   dataNascimento DATE,
+   telefone TEXT,
+   endereco TEXT, 
+   password TEXT
+)`, (err) => {
+  if (err){
+  console.error("erro ao criar a tabela users: ", err);
+  }
+  else{
+  console.log("Tabela users criada com sucesso");
+  }
 });
+
+//criar usuario
+router.post("/register", (req, res) => {
+  const { username , email, cpf, telefone,endereco, password } = req.body;
+  console.log(req.body)
+
+  db.run(
+    "INSERT INTO users (username, email, cpf, telefone,endereco, password) VALUES (?, ?, ?, ?, ?, ?)",
+    [username, email, cpf, telefone,endereco,password],
+    (error) => {
+      if (error) {
+        res.status(500).send(error);
+        return;
+      }
+      else{
+      res.status(201).send(`Usuário ${username} cadastrado com sucesso`);
+      }
+    },
+  );
+});
+
+/* GET USERS. */
+router.get('/', function(req, res, next) {
+  db.all("SELECT * FROM users", (error, rows) => {
+    if (error) {
+      res.status(500).send(error);
+      return;
+    }
+    res.json(rows);
+  });
+});
+
+//GET PELO ID
+router.get('/:id', function(req, res, next) {
+  const{id} = req.params;
+db.get("SELECT * FROM users WHERE id = ?", [id], (error, row) => {
+  if (error) {
+    res.status(500).send(error);
+    return;
+  }
+  res.json(row);
+});
+});
+
+//PUT USERS
+router.put('/:id', function(req, res, next) {
+  const { id } = req.params;
+  const { username, email, cpf, telefone, endereco, password } = req.body;
+  db.run(
+    "UPDATE users SET username = ?, email = ?, cpf = ?, telefone = ?, endereco = ?, password = ? WHERE id = ?",
+    [username, email, cpf, telefone, endereco, password, id],
+    (error) => {
+      if (error) {
+        res.status(500).send(error);
+        return;
+      }
+      res.send(`Usuário ${username} atualizado com sucesso`);
+    }
+  );
+});
+
+router.patch('/:id', function(req, res, next) {
+  const { id } = req.params;
+  const { username, email, cpf, telefone, endereco, password } = req.body;
+  db.run(
+    "UPDATE users SET username = ?, email = ?, cpf = ?, telefone = ?, endereco = ?, password = ? WHERE id = ?",
+    [username, email, cpf, telefone, endereco, password, id],
+    (error) => {
+      if (error) {
+        res.status(500).send(error);
+        return;
+      }
+      res.send(`Usuário ${username} atualizado com sucesso`);
+    }
+  );
+});
+
+
+router.delete('/:id', function(req, res, next) {
+  const{id} = req.params;
+db.run("DELETE FROM users WHERE id = ?", [id], (error, row) => {
+  if (error) {
+    res.status(500).send(error);
+    return;
+  }
+  res.send(`Usuário ${id} deletado com sucesso`);
+});
+});
+
 
 module.exports = router;
